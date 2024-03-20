8d80e850e408ce59b8696e57d8e2de5f5b0d32e1
nsraymond <raymondnsiegbunam@gmail.com>
2024-03-20 03:03:01 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,7 @@ dotenv.config();
 const app = express();
 app.set('trust proxy', true);
 
-app.use(
-    cors({
-      origin: "http://127.0.0.1:5500/",
-      credentials: true,
-    })
-  );
+app.use(cors());
 
 const PORT = process.env.PORT;
 

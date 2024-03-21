63402edb74bb63ab761fe55208213fbb6bcc8472
nsraymond <raymondnsiegbunam@gmail.com>
2024-03-21 11:17:25 +0100

    adjusted the index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { rateLimit } from 'express-rate-limit'
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from "cors";
@@ -12,16 +11,6 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
-
-// Apply the rate limiting middleware to all requests.
-app.use(limiter)
 
 // middlewares
 app.use(bodyParser.json());

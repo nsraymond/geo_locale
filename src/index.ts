d0747cd61030e85945f64475844808cf6f5cb897
nsraymond <raymondnsiegbunam@gmail.com>
2024-03-21 10:20:28 +0100

    implementing ratelimiting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { rateLimit } from 'express-rate-limit'
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from "cors";
@@ -10,6 +11,17 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 // middlewares
 app.use(bodyParser.json());
